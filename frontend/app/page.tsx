0fba8f571c87f6ef795261caf6fd04f885f482f6
Bikram <us08692@gmail.com>
2025-08-22 20:39:53 +0545

    update

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,13 +2,7 @@
 import { useEffect, useState } from "react";
 
 export default function Page() {
-  const [data, setData] = useState<any>(null);
 
-  useEffect(() => {
-    fetch("http://localhost:8000/hello")
-      .then((r) => r.json())
-      .then(setData);
-  }, []);
 
   return (
     <div>
